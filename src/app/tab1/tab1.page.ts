83ea6de11f9cb243dba2533c4305f987b847f7ef
edsson <edssongera@hotmail.com>
2022-10-23 18:36:45 -0600

    Add autofocus && keypressed event

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,15 @@
 import { TasksService } from './../services/tasks.service';
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild} from "@angular/core";
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+  @ViewChild('inputTask', {static:true}) inputTask:IonInput;
 
   public tasks: string[];
   public task: string;
@@ -15,12 +18,13 @@ export class Tab1Page {
     this.tasks = this.taskService.getTasks();
     this.task = "algo";
    }
- 
+
    public addTask() {
      this.taskService.addTask(this.task);
      this.tasks=this.taskService.getTasks();
      console.log(this.tasks);
      this.task="";
+     this.inputTask.setFocus();
    }
  
    public removeTask(pos:number) {
@@ -31,4 +35,10 @@ export class Tab1Page {
    public completeTask(pos: number){
     this.taskService.completeTask(pos);
    }
+
+   ngAfterViewInit() {
+    setTimeout(() => {
+      this.inputTask.setFocus();
+    },500);
+  }
 }
